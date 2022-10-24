d7a95e3c72829dcf1cb5ae60e0c82dfd3c284592
ashmcflash <111534210+ashmcflash@users.noreply.github.com>
2022-10-24 14:28:00 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# golf-lab
+# Golfy-golf-golf
---
+++ scripts/config.js
@@ -0,0 +1,133 @@
+export const games = [
+  {
+    title: "Charlie Lab",
+    link: "https://alecmo26.github.io/CharlieLab/",
+    author: "Alec Moore",
+  },
+  {
+    title: "Echo Lab",
+    link: "https://astricklandd.github.io/echo-lab/",
+    author: "Alex Strickland",
+  },
+  {
+    title: "Echo Lab",
+    link: "https://ashmcflash.github.io/echo-lab/",
+    author: "Ashlyn Paruzynski",
+  },
+  {
+    title: "DOB Facts",
+    link: "https://viii21.github.io/dob-facts/",
+    author: "Chhavi Chhavi",
+  },
+  {
+    title:"Miniature Winner",
+    link: "https://nutenjoyer.github.io/miniature-winner",
+    author: "Cody Mcdonald",
+  },
+  {
+    title: "Professor-Doge",
+    link: "https://almostheaven52.github.io/Professor-Doge/",
+    author: "Zach Newell",
+  },
+  {
+    title: "Delta Lab",
+    link: "https://eaday22.github.io/delta_Lab/",
+    author: "Elijah Aday",
+  },
+  {
+    title: "Delta Lab",
+    link:  "https://etinnon.github.io/cis376-delta/",
+    author: "Emily Tinnon",
+  },
+  {
+    title: "Urban Invention",
+    link:  "https://stalewhitebread.github.io/urban-invention/",
+    author: "Ethan Criddle",
+  },
+  {
+    title: "Delta Lab Name Wow",
+    link:  "https://hubbbabubbba.github.io/Delta-Lab-Name-Wow/",
+    author: "Ethan Hubbard",
+  },
+  {
+    title: "Probable Octo Carnival",
+    link:  "https://crimsonhog.github.io/probable-octo-carnival/",
+    author: "Ethan Rinke",
+  },
+  {
+    title: "Bravo Lab",
+    link:  "https://garrettdodd.tech/CIS-376/BRAVO_LAB/index.html",
+    author: "Garrett Dodd",
+  },
+  {
+    title: "Delta Lab",
+    link:  "https://hauhuynh90.github.io/Delta/",
+    author: "Hau Huynh",
+  },
+  {
+    title: "Charlie Lab",
+    link:  "https://hunter-mcgriff.github.io/dev-lab-charlie/",
+    author: "Hunter McGriff",
+  },
+  {
+    title: "Alpha Lab",
+    link:  "https://codepen.io/katherynisabell/full/WNzWGNd",
+    author: "Katheryn Grant",
+  },
+  {
+    title: "Solid Octo Happiness",
+    link:  "https://hkbrian5.github.io/solid-octo-happiness/", //non working link
+    author: "Kevin Salgado",
+  },
+  {
+    title: "Delta Lab",
+    link:  "https://kwilliams31.github.io/Echo-Cool-Games/",
+    author: "Kristina Williams",
+  },
+  {
+    title: "Echo Lab",
+    link:  "https://lane30schmidt.github.io/echo-376/",
+    author: "Lane Schmidt",
+  },
+  {
+    title: "Delta Lab",
+    link: "https://mlittle98.github.io/DELTALAB/",
+    author: "Maggie Little",
+  },
+  {
+    title: "Sturdy Rotary Phone",
+    link:  "https://mskel1.github.io/sturdy-rotary-phone/",
+    author: "Meghan Skelton",
+  },
+  {
+    title: "Echo Lab",
+    link:  "https://nicolereilly.github.io/echo-lab/",
+    author: "Nicole Reilly",
+  },
+  {
+    title: "Echo Lab",
+    link:  "https://ougheoke.github.io/echo-lab/",
+    author: "Omonegho",
+  },
+  {
+    title: "Charlie Lab",
+    link: "https://panhead63.github.io/Charlie-lab/",
+    author: "Rachel Thompson",
+  },
+  {
+    title: "Doge Game",
+    link: "https://rish4863.github.io/doge-game/",
+    author: "Rishi Doreswamy",
+  },
+  {
+    title: "Delta Lab",
+    link: "https://tythomas21.github.io/delta/",
+    author: "Tychicus Thomas",
+  },
+  {
+    title: "Echo Lab",
+    link: "https://westyler28.github.io/Echo-Lab/",
+    author: "Wes Wallace",
+  },
+  
+];
